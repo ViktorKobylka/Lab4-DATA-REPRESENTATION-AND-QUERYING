5a099fa4d3a08e7c02a5fd243cadebdda2ae7f5b
ViktorKobylka <g00425163@atu.ie>
2024-10-11 14:09:40 +0000

    ex2, task2, added json data, which passed to Movies comp

---
+++ src/components/movies.js
@@ -0,0 +1,11 @@
+const Movies = (props)=>{
+    return(
+        <div>
+            hello from movies component
+            {console.log(props.myMovies)}
+        </div>
+    );
+
+}
+
+export default Movies;
