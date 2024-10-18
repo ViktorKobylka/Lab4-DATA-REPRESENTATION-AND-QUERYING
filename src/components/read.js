d61230bda2f46e333e6a00475f674daf22f8bf7d
ViktorKobylka <g00425163@atu.ie>
2024-10-18 13:44:06 +0000

    ex2 completed

--- src/components/read.js
+++ src/components/read.js
@@ -1,40 +1,29 @@
-//import Movies component
 import Movies from "./movies";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-//define Read component
-const Read = () => {
+function Read() {
+  const [movies, setMovies] = useState([]);
+  const data = [];
+  //give comment
+  useEffect(() => {
+    //give comment
+    axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
-    const data = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          },
-          {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-          },
-          {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-          }
-    ];
+  return (
+    <div>
+      <h2>This is my Read Component.</h2>
+      <Movies myMovies={movies} />
+    </div>
+  );
+}
 
-    
-    return(<div>
-          {/*show heading*/}
-         <h3>Hello from the Read component</h3>
-          {/*pass movie data*/}
-         <Movies myMovies={data}/>
-    </div>);
-  };
-  
-  export default Read;
+export default Read;
